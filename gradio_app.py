99225dc1e62b534ae5bdc2404766ebc67bf5b895
wzq <1781591279@qq.com>
2024-04-04 18:10:41 +0800

    sdf

--- gradio_app.py
+++ gradio_app.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys,os
+
+import pandas as pd
 basedir = os.path.dirname(__file__)
 parent_dir = os.path.dirname(basedir)
 when_import_the_module_the_path=os.path.dirname(__file__)
@@ -29,27 +31,62 @@ sys.path.append(basedir)
 
 
 import gradio as gr
-
-
-
 from pymongo import MongoClient
 
 client = MongoClient(host='139.196.158.152', port=27017, username='root', password='1213wzwz', authSource='admin')
 db = client.zhiqiang_hot
 
+# 查看  修改 两个功能
+# 获取所有mongo的表名
+
+
+all_table_names=db.list_collection_names()
+page_size=30
+
+def get_mongo_skip_page(table_name,pagging):
+    # 获取跳过的条数
+    skip_page=pagging.value*page_size
+    datalist=list(db[table_name].find({}).skip(skip_page).limit(page_size))
+    return datalist
 
 
 with gr.Blocks() as demo:
-    gr.Markdown(
-        """
-        # 基于chatglm-6b的聊天机器人
-        """
-    )
-    gr.Markdown(
-        """
-        ## 输入 
-        """
-    )
+    # 选择关键视图
+    with gr.Row():
+        with gr.Column() as col1:
+            show_or_change=gr.Radio(label='选择关键视图', choices=['查看数据表', '修改数据表', ])
+
+
+    with gr.Row():
+        table_choice=gr.Radio(choices=all_table_names,show_label=False)
+        pagging=gr.Slider(minimum=0,maximum=10,step=1,value=0,show_label=False,interactive=True)
+
+
+    with gr.Column():
+        table_df=gr.Dataframe(pd.DataFrame([]))
+
+    @table_choice.change(inputs=table_choice, outputs= [table_df,pagging])
+    def update_table_df_by_table(table_choice):
+        all_docs_num=db[table_choice].count_documents({})
+        pagging.maximum=all_docs_num//page_size
+        new_pagging=gr.Slider(minimum=0,maximum=pagging.maximum,step=1,value=0,show_label=False,interactive=True)
+
+        data_list=get_mongo_skip_page(table_choice,pagging)
+        new_table_df=pd.DataFrame(data_list)
+        return (new_table_df,new_pagging)
+    
+    @pagging.change(inputs=pagging, outputs= [table_df])
+    def update_table_df_by_pagging(pagging):
+        data_list=get_mongo_skip_page(table_choice.value,pagging)
+        new_table_df=pd.DataFrame(data_list)
+        return (new_table_df)
+    
+
+    with gr.Column():
+        pass
+    with gr.Column():
+        pass
+    
 
 demo.launch(server_port=8888, share=True)
 
